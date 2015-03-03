c3c25624b07cd54e1cd1521a08d2d6a7a2e797ac
andrewdarvell <andrew@darvell.ru>
2015-03-03 15:01:08 +0300

    Немножко говна

--- src/main/java/ru/mmsn/darvell/portal/forms/main/ControllerMain.java
+++ src/main/java/ru/mmsn/darvell/portal/forms/main/ControllerMain.java
@@ -1,20 +1,20 @@
 package ru.mmsn.darvell.portal.forms.main;
 
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 
-import java.net.URL;
-import java.util.ResourceBundle;
 
 /**
- * Created by darvell on 03.03.15.
+ * Контроллер к главной форме
  */
 public class ControllerMain{
 
 	@FXML
 	private Button loginButton;
 
+	/**
+	 * Метод кнопки для авторизации
+	 */
 	@FXML
 	public void onClickMethod(){
 		System.out.println("Button clicked");
